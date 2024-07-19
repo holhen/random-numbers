c3bf360bb2f5abd95a04dfd9e6bd3784eee87f81
holhen <holhen@gmail.com>
2024-07-19 17:27:04 +0200

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,37 @@
+import { useState } from 'react';
+import { getRandomNumber } from './utils';
+import { Button, Card } from 'react-bootstrap';
+import './App.css';
+
+function App() {
+  const initialNumber = getRandomNumber();
+  const [counter, setCounter] = useState(initialNumber);
+
+  const reroll = () => {
+    const number = getRandomNumber();
+    setCounter(number);
+  };
+
+  const increment = () => {
+    setCounter((prev) => prev + 1);
+  };
+
+  const decrement = () => {
+    setCounter((prev) => prev - 1);
+  };
+
+  return (
+    <Card>
+      <Card.Body>
+        <p className="text-primary">{counter}</p>
+      </Card.Body>
+      <Card.Footer>
+        <Button onClick={decrement}>-</Button>
+        <Button onClick={increment}>+</Button>
+        <Button onClick={reroll}>Reroll</Button>
+      </Card.Footer>
+    </Card>
+  );
+}
+
+export default App;
